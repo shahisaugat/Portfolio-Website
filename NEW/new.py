fcbc88ad7671b81e5192c3701571f7c0f9531169
shahi <devops.shahi@gmail.com>
2024-03-06 23:19:02 +0545

    Intruder File removed from the repo

--- NEW/new.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-import random
-import time
-
-while True:
-    code = ''.join(str(random.randint(0, 9)) for _ in range(6))
-    pyautogui.typewrite(code)
-    pyautogui.press('enter')
-    time.sleep(1)
